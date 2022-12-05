a239f01cc2c43d1dd91ccae91164c332d9de6e8e
Mehdi-Ali <alimehdi.offi@gmail.com>
2022-12-05 18:55:14 -0500

    Installing packages
    installed XR packages

---
+++ Assets/Scripts/HMDInfoManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HMDInfoManager : MonoBehaviour
+{
+    private void Start()
+    {
+        var isDeviceActive = XRSettings.isDeviceActive;
+        var loadedDeviceName = XRSettings.loadedDeviceName;
+
+        if (!isDeviceActive)
+        {
+            Debug.Log("No VR Setup detected");
+            return;
+        }
+
+        else if (loadedDeviceName == "Mock HMD" || loadedDeviceName == "MockHMD Display")
+        {
+            Debug.Log("No VR Setup detected, Mocking XR is active");
+        }
+
+        else 
+        {
+            Debug.Log(loadedDeviceName + "Connected");
+        }
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
